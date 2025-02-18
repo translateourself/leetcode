c4f1c0de3cafa092ae3e02fb03d18da51b9a7a71
xu <1119174874@qq.com>
2025-02-18 23:39:48 +0800

    Three Linklist solution.

---
+++ src/leetcode/editor/en/ListNode.java
@@ -0,0 +1,24 @@
+package leetcode.editor.en;/**
+ * @author shkstart
+ * @creative2025-02-18-19:06
+ */
+
+import java.util.ArrayList;
+
+/**
+ * @Description: TODO
+ * @Author: YourName
+ * @Date: 2025/2/18
+ **/
+
+
+
+  public class ListNode {
+         int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        ListNode(ListNode node){}
+     ArrayList<ArrayList> arrays = new ArrayList();
+  }
